a9bd8f763ef2d71877f429c22c5c7050d927f5e7
Tommaso Piazza <tommaso.piazza@gmail.com>
2016-12-05 22:40:32 +0100

    updating binary and sha

--- rome.rb
+++ rome.rb
@@ -1,8 +1,8 @@
 class Rome < Formula
   desc "A shared cache tool for carthage on S3"
   homepage "https://github.com/blender/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.7.1.14/rome.zip"
-  sha256 "3f808d96cb1601a543159264c5b253cd1e070488168441e3e44e5f5bedb549b6"
+  url "https://github.com/blender/Rome/releases/download/v0.7.1.14/rome-0.7.1.14.zip"
+  sha256 "ab1460bf4c02d43c082bbd407cd788a85b42be3b99ba991457fe0cb896eecb85"
   
   version "0.7.1.14"
   
