cf89d8afc61c993ddf7137d9e326e96451d74c55
Tommaso Piazza <tommaso.piazza@gmail.com>
2016-12-05 22:32:02 +0100

    Adding version information

--- rome.rb
+++ rome.rb
@@ -3,7 +3,9 @@ class Rome < Formula
   homepage "https://github.com/blender/Rome"
   url "https://github.com/blender/Rome/releases/download/v0.7.1.14/rome.zip"
   sha256 "3f808d96cb1601a543159264c5b253cd1e070488168441e3e44e5f5bedb549b6"
-
+  
+  version "0.7.1.14"
+  
   bottle :unneeded
 
   def install
