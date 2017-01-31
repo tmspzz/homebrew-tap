9752ec767e3740968f2de48d2735d8c5dfe25a9c
Tommaso Piazza <tommaso.piazza@gmail.com>
2017-01-31 22:40:02 +0100

    Updating rome to version 0.8.0.17 - Local cache support

--- rome.rb
+++ rome.rb
@@ -1,11 +1,11 @@
 class Rome < Formula
   desc "A shared cache tool for carthage on S3"
   homepage "https://github.com/blender/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.7.1.14/rome-0.7.1.14.zip"
-  sha256 "ab1460bf4c02d43c082bbd407cd788a85b42be3b99ba991457fe0cb896eecb85"
-  
-  version "0.7.1.14"
-  
+  url "https://github.com/blender/Rome/releases/download/v0.8.0.17/rome.zip"
+  sha256 "4c599fef0ab4783393bef56cc5d67514c47aa83f5c1819a40f8c74ec2fbc5589"
+
+  version "0.8.0.17"
+
   bottle :unneeded
 
   def install
