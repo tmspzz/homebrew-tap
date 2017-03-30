82ba151e26c59fe69ccbbcaeb0fe7666f47d91ab
Tommaso Piazza <tommaso.piazza@gmail.com>
2017-03-30 18:12:31 +0200

    Update rome to release v0.10.1.22

--- rome.rb
+++ rome.rb
@@ -1,10 +1,10 @@
 class Rome < Formula
   desc "A shared cache tool for carthage on S3"
   homepage "https://github.com/blender/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.10.0.21/rome.zip"
-  sha256 "4ad6c672e5000590d7040872bd083001e001639fa1a7030194964497929f99a1"
+  url "https://github.com/blender/Rome/releases/download/v0.10.1.22/rome.zip"
+  sha256 "4cca57cefbde1d885a81c3c7d01973212c9cd91ea457d2bde2d790025ada93de"
 
-  version "0.10.0.21"
+  version "0.10.1.22"
 
   bottle :unneeded
 
