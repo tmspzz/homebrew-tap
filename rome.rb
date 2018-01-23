de18aaa374c9f22dccf29eca3c2306ca3a40d862
Tommaso Piazza <tommaso.piazza@gmail.com>
2018-01-23 19:34:22 +0100

    Update sha hash

--- rome.rb
+++ rome.rb
@@ -2,7 +2,7 @@ class Rome < Formula
   desc "A shared cache tool for Carthage on S3"
   homepage "https://github.com/blender/Rome"
   url "https://github.com/blender/Rome/releases/download/v0.14.1.39/rome.zip"
-  sha256 "c04672f3384fde6f08ccb8ce1e2d14c5fc98792a5f996a6787457e011b63a2ab"
+  sha256 "b94b1c6ae400c0a892e4c9b5397f8e4a5c9cfa8d21dece9aa494b61f04c17114"
 
   bottle :unneeded
 
