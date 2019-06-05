bd0f23bda64fb5de3b409fcfd00f147122da6a15
Tommaso Piazza <196761+blender@users.noreply.github.com>
2019-06-05 11:16:12 +0200

    Update to 0.23.0.60 (#23)

--- rome.rb
+++ rome.rb
@@ -1,8 +1,8 @@
 class Rome < Formula
   desc "A shared cache tool for Carthage"
   homepage "https://github.com/blender/Rome"
-  url "https://github.com/blender/Rome/releases/download/v0.22.0.59/rome.zip"
-  sha256 "06d05a0ba573d8fb46070b53e8091372feb294f6b30daf8ced784ccd097d25f9"
+  url "https://github.com/blender/Rome/releases/download/v0.23.0.60/rome.zip"
+  sha256 "4c65f05683705c6a7950ccb4707211470a155f4c3453a498ecb2072c303c6243"
 
   bottle :unneeded
 
